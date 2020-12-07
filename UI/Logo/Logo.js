1b906a50607434c4db852ba3f39d967a0f856afc
Rashad Ataf <rashadattaf@gmail.com>
2020-12-07 17:47:41 +0300

    - Replacing the logo

--- UI/Logo/Logo.js
+++ UI/Logo/Logo.js
@@ -3,7 +3,7 @@ import {connect} from 'react-redux'
 import { setCurrentUrl } from '../../store/actions/actions'
 import classes from './Logo.module.css';
 
-const Logo = (props) => <Link href="/"><a onClick={() => props.setCurrentUrl('/')}><img src="/logo.jpg" alt="Tombul Goril Logo" className={classes.Logo} /></a></Link>
+const Logo = (props) => <Link href="/"><a onClick={() => props.setCurrentUrl('/')}><img src="/logo.png" alt="Tombul Goril Logo" className={classes.Logo} /></a></Link>
 
 const mapDispatchToProps = dispatch => {
     return {
